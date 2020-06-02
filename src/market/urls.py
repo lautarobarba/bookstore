bf1e48e6ab2c06eeb5a533c679827b30ce7aba9a
Lautaro Barba <lau_mb94@hotmail.com>
2020-06-02 16:08:45 -0300

    Agregado dashboard para administradores

--- src/market/urls.py
+++ src/market/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from market.views import HomeView
+from market.views import HomeView, DashboardView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
