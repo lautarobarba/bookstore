78cb501011e23dc93df8640fd9b5e341f8cb391f
Lautaro Barba <lau_mb94@hotmail.com>
2020-06-02 04:50:58 -0300

    Rutas para los templates principales y los archivos static principales

--- src/bookstore/settings.py
+++ src/bookstore/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'market.apps.MarketConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Custom User
+# https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#substituting-a-custom-user-model
+AUTH_USER_MODEL = 'users.User'
+
+# Login url used by LoginRequired mixin
+LOGIN_URL = '/user/login/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -118,3 +127,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+# Media files
+# MEDIA_ROOT is the absolute filesystem path to the directory for user-uploaded files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL is the URL we can use in our templates for the files
+MEDIA_URL = '/media/'
+# In order to use MEDIA_URL you should add this to your ulr.py main file
+#from django.conf import settings
+#from django.conf.urls.static import static
+#if settings.DEBUG:
+#   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# Crispy template pack
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
