e33d992e0bdeaeed409aa78c09b99f7f44f11fb3
tom-villanueva <54720888+tom-villanueva@users.noreply.github.com>
2020-06-14 18:17:23 -0300

    sidebar created

--- src/market/views/dashboard.py
+++ src/market/views/dashboard.py
@@ -2,4 +2,4 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
-    template_name = 'dashboard.html'
+    template_name = 'market/book_create.html'
