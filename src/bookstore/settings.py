dd546b98b571d68e2bde41ba0c66dd14ef24131b
Lautaro Barba <lau_mb94@hotmail.com>
2020-06-14 21:04:57 -0300

    Se cambio el directorio de collectstatic

--- src/bookstore/settings.py
+++ src/bookstore/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = '/home/lautaro/static'
+STATIC_ROOT = '/home/admin/static'
 
 
 # Media files
