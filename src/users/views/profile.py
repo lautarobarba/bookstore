3878bbe8ad7bb1e83dfdcfd4109360e96f69f57c
Lautaro Barba <lau_mb94@hotmail.com>
2020-06-18 12:57:53 -0300

    LoginRequiredMixin en vistas de modificacion de perfil

--- src/users/views/profile.py
+++ src/users/views/profile.py
@@ -3,6 +3,7 @@ from users.models import Profile
 from django.views.generic.edit import UpdateView
 from django.views.generic import ListView
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Custom mixins
 from .mixins import ProfileOwnerMixin
@@ -14,12 +15,12 @@ User = get_user_model()
 class ProfileDetailView(ProfileOwnerMixin, DetailView):
     model = Profile
 
-class ProfileUpdateView(ProfileOwnerMixin, UpdateView):
+class ProfileUpdateView(ProfileOwnerMixin, LoginRequiredMixin, UpdateView):
     model = Profile
     template_name = 'users/profile_update.html'
     fields = ['first_name', 'last_name', 'country', 'phone', 'picture']
 
-class ProfileGroupUpdateView(ProfileOwnerMixin, UpdateView):
+class ProfileGroupUpdateView(ProfileOwnerMixin, LoginRequiredMixin, UpdateView):
     model = Profile
     template_name = 'users/profile_group_update.html'
     fields = ['group']
