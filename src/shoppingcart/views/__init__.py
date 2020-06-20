114bdfcffc116196ccdda977d149bf6d38abb856
tom-villanueva <54720888+tom-villanueva@users.noreply.github.com>
2020-06-20 18:51:50 -0300

    corregido redirect  shopping_cart

--- src/shoppingcart/views/__init__.py
+++ src/shoppingcart/views/__init__.py
@@ -1,5 +1,5 @@
 from .cartdetailview import CartDetailView
-from .cart_functions import add_to_cart, remove_from_cart
+from .cart_functions import add_to_cart, remove_from_cart, add_to_cart_from_wishlist
 from .order import (
     OrderCreateView, OrderListView, OrderDetailView, ResumeView,
     PrintOrderView,
